e93cd4405958ff9db7b8a12e7a9cdecd5529b33e
Aleksandr Serenko <fafnur@yandex.ru>
2022-02-05 13:57:04 +0700

    [Vendors][Universal] Adding angular universal

--- apps/store/src/app/app-core.module.ts
+++ apps/store/src/app/app-core.module.ts
@@ -1,11 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
 
 @NgModule({
-  imports: [HttpClientModule, !environment.production ? StoreDevtoolsModule.instrument({ logOnly: environment.production }) : []],
+  imports: [
+    HttpClientModule,
+    !environment.production ? StoreDevtoolsModule.instrument({ logOnly: environment.production }) : [],
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
 })
 export class AppCoreModule {}
--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { AppCoreModule } from './app-core.module';
 
 @NgModule({
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppCoreModule],
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppCoreModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([])],
-  providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
