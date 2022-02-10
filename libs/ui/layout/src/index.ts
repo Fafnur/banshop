7c68fa62e2085223cdbf6defe901e359b925ddc1
Aleksandr Serenko <fafnur@yandex.ru>
2022-02-10 08:18:37 +0700

    [Ui][Container] Customizing layout type logic

--- libs/ui/layout/src/index.ts
+++ libs/ui/layout/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/layout.component';
 export * from './lib/layout.module';
+export * from './lib/layout.service';
--- libs/ui/layout/src/lib/layout.component.ts
+++ libs/ui/layout/src/lib/layout.component.ts
@@ -1,53 +1,23 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { takeUntil, tap } from 'rxjs';
+import { Breakpoints } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { DestroyService } from '@banshop/core/utils/destroy';
-
-export const BREAKPOINT_TYPES = {
-  [Breakpoints.Handset]: Breakpoints.Handset,
-  [Breakpoints.HandsetPortrait]: Breakpoints.Handset,
-  [Breakpoints.HandsetLandscape]: Breakpoints.Handset,
-  [Breakpoints.Tablet]: Breakpoints.Tablet,
-  [Breakpoints.TabletPortrait]: Breakpoints.Tablet,
-  [Breakpoints.TabletLandscape]: Breakpoints.Tablet,
-  [Breakpoints.Web]: Breakpoints.Web,
-  [Breakpoints.WebPortrait]: Breakpoints.Web,
-  [Breakpoints.WebLandscape]: Breakpoints.Web,
-};
+import { LayoutService } from './layout.service';
 
 @Component({
   selector: 'banshop-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [DestroyService],
 })
 export class LayoutComponent implements OnInit {
   readonly breakpoints = Breakpoints;
 
-  layoutType = Breakpoints.Handset;
+  layoutType$!: Observable<string>;
 
-  constructor(
-    private readonly changeDetectorRef: ChangeDetectorRef,
-    private readonly breakpointObserver: BreakpointObserver,
-    private readonly destroy$: DestroyService
-  ) {}
+  constructor(private readonly layoutService: LayoutService) {}
 
   ngOnInit(): void {
-    this.breakpointObserver
-      .observe([Breakpoints.Handset, Breakpoints.Tablet, Breakpoints.Web])
-      .pipe(
-        tap((result) => {
-          for (const query of Object.keys(result.breakpoints)) {
-            if (result.breakpoints[query]) {
-              this.layoutType = BREAKPOINT_TYPES[query];
-            }
-          }
-          this.changeDetectorRef.markForCheck();
-        }),
-        takeUntil(this.destroy$)
-      )
-      .subscribe();
+    this.layoutType$ = this.layoutService.layoutType$;
   }
 }
