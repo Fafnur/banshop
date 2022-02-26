ad6ba8a2614f2b6c15c5aeb154d0cb7123f22745
Aleksandr Serenko <fafnur@yandex.ru>
2022-02-26 09:51:45 +0700

    [Core][Forms] Adding core forms utils and extracts

---
+++ libs/core/forms/extract/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/extract-form-control.pipe';
+export * from './lib/extract-form-group.pipe';
+export * from './lib/extract-touched.directive';
+export * from './lib/form-extracts.module';
---
+++ libs/core/forms/utils/README.md
@@ -0,0 +1,7 @@
+# core-forms-utils
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test core-forms-utils` to execute the unit tests.
