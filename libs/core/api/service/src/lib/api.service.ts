1021f73ba54a14bb56d123fc8ec28786d6877cc8
Aleksandr Serenko <fafnur@yandex.ru>
2022-03-11 08:37:52 +0700

    [Core][] Optimization api service

--- libs/core/api/service/src/lib/api.service.ts
+++ libs/core/api/service/src/lib/api.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { ApiRequestOptions, getApiRequestOptions } from './api.util';
 
@@ -16,24 +15,22 @@ export class ApiService {
   }
 
   get<T = void>(url: string, options?: Partial<ApiRequestOptions>): Observable<T> {
-    return this.httpClient.get<T>(this.makeUrl(url), getApiRequestOptions(options)).pipe(catchError((error) => throwError(error)));
+    return this.httpClient.get<T>(this.makeUrl(url), getApiRequestOptions(options));
   }
 
   post<T = void>(url: string, body?: unknown | null, options?: Partial<ApiRequestOptions>): Observable<T> {
-    return this.httpClient
-      .post<T>(this.makeUrl(url), body ?? null, getApiRequestOptions(options))
-      .pipe(catchError((error) => throwError(error)));
+    return this.httpClient.post<T>(this.makeUrl(url), body ?? null, getApiRequestOptions(options));
   }
 
   patch<T = void>(url: string, body: unknown | null, options?: Partial<ApiRequestOptions>): Observable<T> {
-    return this.httpClient.patch<T>(this.makeUrl(url), body, getApiRequestOptions(options)).pipe(catchError((error) => throwError(error)));
+    return this.httpClient.patch<T>(this.makeUrl(url), body, getApiRequestOptions(options));
   }
 
   put<T = void>(url: string, body: unknown | null, options?: Partial<ApiRequestOptions>): Observable<T> {
-    return this.httpClient.put<T>(this.makeUrl(url), body, getApiRequestOptions(options)).pipe(catchError((error) => throwError(error)));
+    return this.httpClient.put<T>(this.makeUrl(url), body, getApiRequestOptions(options));
   }
 
   delete<T = void>(url: string, options?: Partial<ApiRequestOptions>): Observable<T> {
-    return this.httpClient.delete<T>(this.makeUrl(url), getApiRequestOptions(options)).pipe(catchError((error) => throwError(error)));
+    return this.httpClient.delete<T>(this.makeUrl(url), getApiRequestOptions(options));
   }
 }
