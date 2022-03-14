08a40518d6fd26733a656506b25da5ea963f0300
Aleksandr Serenko <fafnur@yandex.ru>
2022-03-15 00:11:08 +0700

    [Chat][Ui] Adding tests for chat pipes

--- libs/chat/ui/pipes/src/lib/chat-icon.pipe.spec.ts
+++ libs/chat/ui/pipes/src/lib/chat-icon.pipe.spec.ts
@@ -1,8 +1,18 @@
 import { ChatIconPipe } from './chat-icon.pipe';
 
 describe('ChatIconPipe', () => {
+  let pipe: ChatIconPipe;
+
+  beforeEach(() => {
+    pipe = new ChatIconPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new ChatIconPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('create return result', () => {
+    expect(pipe.transform(true)).toBe(`/assets/images/user.svg`);
+    expect(pipe.transform(false)).toBe(`/assets/images/logo.svg`);
   });
 });
