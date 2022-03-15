3988c99da7ee139bcd44c56440e013e03864a153
Aleksandr Serenko <fafnur@yandex.ru>
2022-03-15 09:13:08 +0700

    [Home][Tests] Adding tests for home page

--- libs/home/page/src/lib/home-page.component.spec.ts
+++ libs/home/page/src/lib/home-page.component.spec.ts
@@ -3,29 +3,36 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockModule } from 'ng-mocks';
 
-import { ContainerModule } from '@banshop/ui/container';
+import { ProductListModule } from '@banshop/products/ui/list';
 
 import { HomePageComponent } from './home-page.component';
+import { HomePageComponentPo } from './home-page.component.po';
 
 describe('HomePageComponent', () => {
-  let component: HomePageComponent;
+  let pageObject: HomePageComponentPo;
   let fixture: ComponentFixture<HomePageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CommonModule, RouterTestingModule, MockModule(ContainerModule)],
+      imports: [CommonModule, RouterTestingModule, MockModule(ProductListModule)],
       declarations: [HomePageComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomePageComponent);
-    component = fixture.componentInstance;
+    pageObject = new HomePageComponentPo(fixture);
   });
 
   it('should create', () => {
     fixture.detectChanges();
 
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should show', () => {
+    fixture.detectChanges();
+
+    expect(pageObject.list).toBeTruthy();
   });
 });
--- libs/home/page/src/lib/home-page.module.ts
+++ libs/home/page/src/lib/home-page.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ProductListModule } from '@banshop/products/ui/list';
-import { ContainerModule } from '@banshop/ui/container';
 
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
 
 @NgModule({
-  imports: [CommonModule, HomePageRoutingModule, ContainerModule, ProductListModule],
+  imports: [CommonModule, HomePageRoutingModule, ProductListModule],
   declarations: [HomePageComponent],
 })
 export class HomePageModule {}
