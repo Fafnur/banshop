23212108440e7e9df999d765729c99317438a60b
Aleksandr Serenko <fafnur@yandex.ru>
2022-03-26 01:13:08 +0700

    [Localization][Translate] Fixing localization errors

--- libs/russian/localization/src/lib/mat-data-adapter.ts
+++ libs/russian/localization/src/lib/mat-data-adapter.ts
@@ -3,8 +3,8 @@ import { NativeDateAdapter } from '@angular/material/core';
 
 @Injectable()
 export class RussianMatDataAdapter extends NativeDateAdapter {
-  override parse(value: any): Date | null {
-    const dateParts: string[] = value ? value.split('.') : [];
+  override parse(value: unknown): Date | null {
+    const dateParts: string[] = typeof value === 'string' ? value.split('.') : [];
 
     if (dateParts.length === 3) {
       return super.parse(dateParts.reverse().join('-'));
